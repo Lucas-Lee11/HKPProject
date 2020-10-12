43f26581435673d5600dc58ca6855edbe2b80161
Lucas Lee <lucasclee11@gmail.com>
2020-10-12 18:52:34 -0400

    start

---
+++ FinalProject/FinalProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FinalProject
+//
+//  Created by Lucas Lee on 10/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
